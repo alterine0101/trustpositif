210f5d2f963a1210254ed044661ea0ccf42569a9
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2022-12-06 13:06:11 +0700

    feat: Add reverse regex, fix issues

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 	"os"
 	"sort"
 	"strings"
+	"sync"
 )
 
 type Node struct {
 	char             string
 	children         NodeChildren
+	lastLetter       bool
 	optionalChildren NodeChildren
 	parent           *Node
 	weight           int
@@ -19,22 +21,35 @@ type Node struct {
 
 type NodeChildren [40]*Node
 
+var insertWaitGroup sync.WaitGroup
+
+func reverse(s string) string {
+	rns := []rune(s) // convert to rune
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+	return string(rns)
+}
+
 func NewNode(char string, parent *Node) *Node {
-	node := &Node{char: char, parent: parent, weight: 1}
+	node := &Node{char: char, lastLetter: false, parent: parent, weight: 1}
 	for i := 0; i < 40; i++ {
 		node.children[i] = nil
 	}
 	return node
 }
 
-func (node *Node) Insert(insert string) {
-	if node == nil {
-		node = NewNode(insert, nil)
+func (node *Node) Insert(insert string, withWaitGroup bool) {
+	if withWaitGroup {
+		insertWaitGroup.Add(1)
 	}
+	first := insert[0:1]
 	if len(insert) == 0 {
 		return
 	}
-	first := insert[0:1]
+	if node == nil {
+		node = NewNode("\000", nil)
+	}
 	lastIndex := -1
 	found := false
 	for i, child := range &node.children {
@@ -51,7 +66,12 @@ func (node *Node) Insert(insert string) {
 		node.children[lastIndex] = NewNode(first, node)
 	}
 	if len(insert) > 1 {
-		node.children[lastIndex].Insert(insert[1:])
+		node.children[lastIndex].Insert(insert[1:], false)
+	} else {
+		node.children[lastIndex].lastLetter = true
+	}
+	if withWaitGroup {
+		insertWaitGroup.Done()
 	}
 }
 
@@ -73,83 +93,159 @@ DO:
 	}
 DONE:
 	node = &cursor
+	sort.Sort(node.children)
 	for i := 0; i < 40; i++ {
 		node.children[i] = OptimizeSubtrie(node.children[i])
 	}
-	sort.Sort(node.children)
+	if node.lastLetter == true {
+		// Move everything to optionals
+		// fmt.Printf("| Letter %s has optionals\n", node.char)
+		for i := 0; i < 40; i++ {
+			node.optionalChildren[i] = node.children[i]
+			node.children[i] = nil
+		}
+	}
 	return node
 }
 
-func (node *Node) GenerateRegex() string {
+func PrintChar(char string, file *os.File) {
+	if char == "." {
+		_, err2 := file.WriteString("\\.")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	} else if char == "?" {
+		_, err2 := file.WriteString("\\?")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	} else if char == "*" {
+		_, err2 := file.WriteString(".")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	} else {
+		_, err2 := file.WriteString(char)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	}
+}
+
+func (node *Node) GenerateRegex(file *os.File) {
 	if node == nil {
-		return ""
+		return
 	}
 
-	res := ""
+	optionalParentChildrenSize := 0
+	parentChildrenSize := 0
+	optionalChildrenSize := 0
+	childrenSize := 0
 
-	optionalChildrenSize := node.optionalChildren.Len()
-	childrenSize := node.children.Len()
-	shouldPrintContainerBracket := node.parent != nil && node.parent.children.Len() > 1
+	for i := 0; i < 40; i++ {
+		if node.parent != nil {
+			if node.parent.optionalChildren[i] != nil && node.parent.optionalChildren[i].char != "\000" {
+				optionalParentChildrenSize++
+			}
+			if node.parent.children[i] != nil && node.parent.children[i].char != "\000" {
+				parentChildrenSize++
+			}
+		}
+		if node.optionalChildren[i] != nil && node.optionalChildren[i].char != "\000" {
+			optionalChildrenSize++
+		}
+		if node.children[i] != nil && node.children[i].char != "\000" {
+			childrenSize++
+		}
+	}
+
+	shouldPrintContainerBracket := node.parent != nil && optionalParentChildrenSize > 1
 
 	if shouldPrintContainerBracket {
-		res += "("
+		_, err2 := file.WriteString("(")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
 	}
 
 	if node.char != "\000" {
-		if node.char == "." {
-			res += "\\."
-		} else if node.char == "?" {
-			res += "\\?"
-		} else if node.char == "*" {
-			res += "."
-		} else {
-			res += node.char
-		}
+		PrintChar(node.char, file)
 		if node.weight > 1 {
-			res += fmt.Sprintf("{%d}", node.weight)
+			if node.char == "." || node.char == "?" || node.weight > 5 {
+				_, err2 := file.WriteString(fmt.Sprintf("{%d}", node.weight))
+				if err2 != nil {
+					log.Fatal(err2)
+				}
+			} else {
+				for i := 1; i < node.weight; i++ {
+					PrintChar(node.char, file)
+				}
+			}
 		}
 	}
 
 	if optionalChildrenSize > 0 {
 		if optionalChildrenSize > 1 {
-			res += "("
+			_, err2 := file.WriteString("(")
+			if err2 != nil {
+				log.Fatal(err2)
+			}
 		}
 		for i, child := range &node.optionalChildren {
 			if child == nil {
 				break
 			}
 			if i > 0 {
-				res += "|"
+				_, err2 := file.WriteString("|")
+				if err2 != nil {
+					log.Fatal(err2)
+				}
 			}
-			res += child.GenerateRegex()
+			child.GenerateRegex(file)
 		}
 		if optionalChildrenSize > 1 {
-			res += ")"
+			_, err2 := file.WriteString(")")
+			if err2 != nil {
+				log.Fatal(err2)
+			}
 		}
-		res += "{0,1}"
+		_, err2 := file.WriteString("{0,1}")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
 	}
 
 	if childrenSize > 1 {
-		res += "("
+		_, err2 := file.WriteString("(")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
 	}
 	for i, child := range &node.children {
 		if child == nil {
 			break
 		}
 		if i > 0 {
-			res += "|"
+			_, err2 := file.WriteString("|")
+			if err2 != nil {
+				log.Fatal(err2)
+			}
 		}
-		res += child.GenerateRegex()
+		child.GenerateRegex(file)
 	}
 	if childrenSize > 1 {
-		res += ")"
+		_, err2 := file.WriteString(")")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
 	}
 
 	if shouldPrintContainerBracket {
-		res += ")"
+		_, err2 := file.WriteString(")")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
 	}
-
-	return res
 }
 
 var root *Node = NewNode("\000", nil)
@@ -176,7 +272,13 @@ func (children NodeChildren) Swap(i, j int) {
 }
 
 func main() {
+	reversed := len(os.Args) > 1 && os.Args[1] == "--reverse"
+	if reversed {
+		println("* Reverse trie used")
+	}
+
 	/* Stage 1: Import all domains list */
+	println("* Stage 1 start")
 	inputFile, err := os.Open("input/domains")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -190,29 +292,40 @@ func main() {
 		domains++
 		text := scanner.Text()
 		text = strings.ToLower(text)
-		root.Insert(text)
-
+		if reversed {
+			text = reverse(text)
+		}
+		go root.Insert(text, true)
 		if err := scanner.Err(); err != nil {
 			fmt.Println(err)
 		}
+		if domains%250000 == 0 {
+			fmt.Printf("| %d domains imported to the freakin' huge trie\n", domains)
+		}
 	}
+	fmt.Printf("| %d domains imported to the freakin' huge trie\n", domains)
 	inputFile.Close()
-	// fmt.Printf("| %d domains imported to the freakin' huge trie\n", domains)
+	insertWaitGroup.Wait()
+	fmt.Println("| All goroutines have been cleared")
 
 	/* Stage 2: Simplify */
+	println("* Stage 2 start")
 	root := OptimizeSubtrie(root)
-	regex := root.GenerateRegex()
 
-	outputFile, err := os.Create("output/regex.txt")
-
+	/* Stage 3: Generate Regex */
+	println("* Stage 3 start")
+	var fileName string
+	if reversed {
+		fileName = "output/regex-reversed.txt"
+	} else {
+		fileName = "output/regex.txt"
+	}
+	outputFile, err := os.Create(fileName)
+	root.GenerateRegex(outputFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err2 := outputFile.WriteString(regex)
-
-	if err2 != nil {
-		log.Fatal(err2)
-	}
+	println("| Finished writing file")
 
 	outputFile.Close()
 	fmt.Println("done")
--- main_test.go
+++ main_test.go
@@ -73,3 +73,67 @@ func TestRegexResult(t *testing.T) {
 	outputFile.Close()
 	t.Log("done")
 }
+
+func TestReverseRegexResult(t *testing.T) {
+	/* Stage 1: Import all domains list */
+	t.Log("Opening file...")
+	inputFile, err := os.Open("input/domains")
+	regexFile, err2 := ioutil.ReadFile("output/regex-reversed.txt")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+		return
+	}
+	if err2 != nil {
+		t.Log(err2.Error())
+		t.Fail()
+		return
+	}
+
+	scanner := bufio.NewScanner(inputFile)
+	correct := 0
+	domains := 0
+	var undetected []string
+
+	t.Log("Compiling regex...")
+	regex := string(regexFile)
+	matcher := regexp2.MustCompile(regex, 1)
+	t.Log("Starting validation...")
+
+	for scanner.Scan() {
+		domain := scanner.Text()
+		val, err := matcher.MatchString(reverse(domain))
+		if val {
+			correct++
+		} else {
+			undetected = append(undetected, domain)
+		}
+		err = scanner.Err()
+		domains++
+		if err != nil {
+			t.Log(err)
+			t.Fail()
+		}
+		if domains%50000 == 0 {
+			t.Logf("%d out of %d correct (%.2f percent)", correct, domains, float32(correct)/float32(domains)*100)
+		}
+	}
+
+	t.Logf("%d out of %d correct (%.2f percent)", correct, domains, float32(correct)/float32(domains)*100)
+	outputFile, err := os.Create("output/undetected-reversed.txt")
+
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	_, err3 := outputFile.WriteString(strings.Join(undetected, "\n"))
+
+	if err3 != nil {
+		t.Log(err3)
+		t.Fail()
+	}
+
+	inputFile.Close()
+	outputFile.Close()
+	t.Log("done")
+}
